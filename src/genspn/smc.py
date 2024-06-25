fix: likelihood masking bug

src/genspn/smc.py
@@ -24,22 +24,32 @@ def get_weights(trace, K, data, q_split_trace, max_clusters):
     # I think the idea will be to make a separate pi vector for each 
     # possible cluster split
     pi_split = jax.vmap(make_pi, in_axes=(None, 0, None, None))(
-        trace.pi, k, q_split_trace.pi, max_clusters)
-    logpdf_pi = jax.vmap(logpdf, in_axes=(None, 0, None))(trace.GEM, pi_split, K)
+        trace.pi, jnp.arange(max_clusters), q_split_trace.pi[-1], max_clusters)
+    logpdf_pi = jax.vmap(logpdf, in_axes=(None, 0, None))(trace.gem, pi_split, K)
 
     logpdf_clusters = score_trace_cluster(data, trace.g, trace.cluster, trace)
     logpdf_except_cluster = jnp.sum(logpdf_clusters) - logpdf_clusters
 
+    logpdf_q_pi = score_q_pi(q_split_trace.pi, max_clusters, trace.gem.alpha)   
+
     # score pi1 and pi2 under each proposal
-    q_pi_dist = Dirichlet(alpha=jnp.ones(2) * trace.gem.alpha/2)
-    q_pi = jnp.vstack((
-        q_split_trace.cluster.pi[jnp.arange(K)],
-        q_split_trace.cluster.pi[max_clusters + jnp.arange(K)],
-    ))
-
-    logpdf_q_pi = jax.vmap(logpdf, in_axes=(None, 0))(q_pi_dist, q_pi)
-
     return logpdf_pi + logpdf_except_cluster - logpdf_q_pi
+
+def score_q_pi(q_pi, max_clusters, alpha):
+    q_pi_dist = Dirichlet(alpha=jnp.ones(2) * alpha/2)
+    q_pi = Categorical(jnp.vstack((
+        q_pi[:max_clusters],
+        q_pi[max_clusters:],
+    )))
+
+    return jax.vmap(logpdf, in_axes=(None, 0))(q_pi_dist, q_pi)
+
+def make_pi(pi, k, pi_split, max_clusters):
+    pi_k0 = pi[k]
+    pi = pi.at[k].set(pi_k0 * pi_split[k])
+    pi = pi.at[k + max_clusters].set(pi_k0 * pi_split[k + max_clusters])
+
+    return jnp.sort(pi, descending=True)
 
 def score_trace_cluster(data, g, cluster, max_clusters):
     c, pi, f = cluster.c, cluster.pi, cluster.f
@@ -58,16 +68,16 @@ def score_trace_cluster(data, g, cluster, max_clusters):
 def split_cluster(trace, k):
     pass
 
-@partial(jax.jit, static_argnames=['data', 'gibbs_iters', 'max_clusters'])
-def q_split(data, gibbs_iters, max_clusters, key, trace) -> Cluster:
-    c0 = trace.cluster.c
+@partial(jax.jit, static_argnames=['gibbs_iters', 'max_clusters'])
+def q_split(data, gibbs_iters, max_clusters, key, c0, alpha, g) -> Cluster:
     keys = jax.random.split(key, 3)
     c = (c0 + max_clusters * jax.random.bernoulli(keys[0], shape=c0.shape)).astype(int)
+    # c = jnp.concatenate((jnp.zeros(100), jnp.ones(100) * 2))
 
-    log_likelihood_mask = make_log_likelihood_mask(c, max_clusters)
+    log_likelihood_mask = make_log_likelihood_mask(c0, max_clusters)
 
     partial_gibbs_step = partial(gibbs_step, 
-        alpha=trace.gem.alpha, g=trace.g, data=data, 
+        alpha=alpha, g=g, data=data, 
         log_likelihood_mask=log_likelihood_mask, max_clusters=max_clusters)
 
     keys = jax.random.split(keys[2], gibbs_iters)
@@ -83,25 +93,29 @@ def make_log_likelihood_mask(c, max_clusters):
     clusters_y = jnp.concatenate((c, max_clusters + c), dtype=int)
     return log_likelihood_mask.at[clusters_x, clusters_y].set(0)
 
-def gibbs_step(c, key, alpha, g, data, log_likelihood_mask, max_clusters):
+def gibbs_step(assignments, key, alpha, g, data, log_likelihood_mask, max_clusters):
     subkey1, subkey2, subkey3 = jax.random.split(key, 3)
-    f = gibbs_f(max_clusters, data, subkey1, g, c)
-    pi = gibbs_pi(max_clusters, subkey2, alpha, c)
-    c = gibbs_c(subkey3, pi, log_likelihood_mask, f, data)
+    f = gibbs_f(max_clusters, data, subkey1, g, assignments)
+    # pi = gibbs_pi(max_clusters, subkey2, alpha, c)
+    pi = jnp.array([.5, .5, .5, .5])
+
+    assignments = gibbs_c(subkey3, pi, log_likelihood_mask, f, data)
 
     # now compute the logpdf for the assignments given the new distribution
-    return c, Cluster(c, pi, f)
+    return assignments, Cluster(assignments, pi, f)
 
-def gibbs_f(max_clusters, data, key, g, c):
-    g_prime = posterior(g, data, c, 2*max_clusters)
-    return sample(key, g_prime)
+def gibbs_f(max_clusters, data, key, g, assignments):
+    g_prime = posterior(g, data, assignments, 2*max_clusters)
+    f = sample(key, g_prime)
+    return f
 
 def gibbs_c(key, pi, log_likelihood_mask, f, data):
     log_likelihoods = jax.vmap(jax.vmap(logpdf, in_axes=(0, None)), in_axes=(None, 0))(f, data)
     log_likelihoods = log_likelihoods + log_likelihood_mask
     log_score = log_likelihoods + jnp.log(pi)
 
-    return jax.random.categorical(key, log_score, axis=-1).astype(int)
+    assignments = jax.random.categorical(key, log_score, axis=-1).astype(int)
+    return assignments
 
 def gibbs_pi(max_clusters, key, alpha, c):
     cluster_counts = jnp.sum(jax.nn.one_hot(c, num_classes=2 * max_clusters, dtype=jnp.int32), axis=0)