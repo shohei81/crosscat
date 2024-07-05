67a2c5f5cec6cc128c562812c9a463647e9959ca
joaoloula <jloula@mit.edu>
2024-07-05 16:36:14 -0400

    feat: transform csv categoricals to int

---
+++ src/genspn/io.py
@@ -0,0 +1,13 @@
+import polars as pl
+import numpy as np
+
+# convert csv to numpy array
+def csv_to_array(file, categorical_vars):
+    df = pl.read_csv(file)
+    categorical_df = df.select(categorical_vars)
+    numerical_df = df.select(pl.exclude(categorical_vars))
+
+    categorical_df = categorical_df.cast(pl.Categorical)
+    numbered_categorical_df = categorical_df.with_columns(pl.all().to_physical())
+
+    return (numerical_df.to_numpy(), numbered_categorical_df.to_numpy())
