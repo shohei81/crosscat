4977b580b3139b5284282009991770f92ff957bd
joaoloula <jloula@mit.edu>
2024-07-07 09:34:22 -0400

    feat: converting dataset categoricals to int

--- src/genspn/io.py
+++ src/genspn/io.py
@@ -10,4 +10,4 @@ def csv_to_array(file, categorical_vars):
     categorical_df = categorical_df.cast(pl.Categorical)
     numbered_categorical_df = categorical_df.with_columns(pl.all().to_physical())
 
-    return (numerical_df.to_numpy(), numbered_categorical_df.to_numpy())
+    return [numerical_df.to_numpy(), numbered_categorical_df.to_numpy()]
