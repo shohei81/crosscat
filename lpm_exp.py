476061c507afeacb7a7a2bcd539889ef68540868
João Loula <jloula@mit.edu>
2024-08-22 18:00:43 +0000

    feat: loading + converting train and test jointly with polars, refactored IO

---
+++ lpm_exp.py
@@ -0,0 +1,6 @@
+# %%
+from genspn.io import load_huggingface
+
+train_data, test_data = load_huggingface("AutoML/heart-disease")
+
+# %%
--- src/genspn/io.py
+++ src/genspn/io.py
@@ -1,13 +1,38 @@
 import polars as pl
+import polars.selectors as cs
 import numpy as np
+from huggingface_hub import login
+import os
 
-# convert csv to numpy array
-def csv_to_array(file, categorical_vars):
-    df = pl.read_csv(file)
-    categorical_df = df.select(categorical_vars)
-    numerical_df = df.select(pl.exclude(categorical_vars))
 
-    categorical_df = categorical_df.cast(pl.Categorical)
-    numbered_categorical_df = categorical_df.with_columns(pl.all().to_physical())
+def dataframe_to_arrays(df: pl.DataFrame):
+    categorical_df = df.select(~cs.numeric())
+    numerical_df = df.select(cs.numeric())
 
-    return [numerical_df.to_numpy(), numbered_categorical_df.to_numpy()]
+    categorical_df = categorical_df_to_integer(categorical_df)
+    return numerical_df.to_numpy(), categorical_df.to_numpy()
+
+def categorical_df_to_integer(df: pl.DataFrame):
+    # TODO get category names
+    df = df.cast(pl.Categorical)
+    return df.with_columns(pl.all().to_physical())
+
+def load_huggingface(dataset_name):
+    # Check if the Hugging Face token is set in the environment variables
+    os.environ["HF_KEY"] = "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"
+    login(token=os.environ.get("HF_KEY"))
+    
+    splits = {
+        "train": f"data/{dataset_name}/data-train.parquet",
+        "test": f"data/{dataset_name}/data-test.parquet"
+    }
+    # XXX: change it to load both later; use same string -> int mapping for both.
+    train_df = pl.read_parquet(f"hf://datasets/Large-Population-Model/model-building-evaluation/{splits['train']}")
+    test_df = pl.read_parquet(f"hf://datasets/Large-Population-Model/model-building-evaluation/{splits['test']}")
+
+    df = pl.concat((train_df, test_df))
+    numerical_array, categorical_array = dataframe_to_arrays(df)
+
+    n_train = len(train_df)
+
+    return (numerical_array[:n_train], categorical_array[:n_train]), (numerical_array[n_train:], categorical_array[n_train:])
