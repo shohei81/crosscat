26584fcad41ed244427b08050260936e9ce5aa0d
Ian Limarta <ian.limarta@gmail.com>
2025-02-04 12:51:48 -0600

    Remove files

--- test/test_sufficient_statistics.py
+++ test/test_sufficient_statistics.py
@@ -3,16 +3,22 @@ import jax.numpy as jnp
 
 import genjaxmix.conjugacy.sufficient_statistics as ss
 
+
 def test_sss_nig_normal():
-    a_0  = jnp.ones(2)
+    a_0 = jnp.ones(2)
     b_0 = jnp.ones(2)
     mu_0 = jnp.zeros(2)
     v_0 = jnp.ones(2)
 
-    x = jnp.array([0.0,1.0,1.0])
-    assignments = jnp.array([0,0,1], dtype=jax.numpy.int32)
+    x = jnp.array([0.0, 1.0, 1.0])
+    assignments = jnp.array([0, 0, 1], dtype=jax.numpy.int32)
     output = ss._sss_nig_normal((a_0, b_0, mu_0, v_0), x, assignments)
-    expected = (jnp.array([2/3, 1]), jnp.array([1/3, 1/2]), jnp.array([1, 1/2]), jnp.array([4/3, 1.0]))
+    expected = (
+        jnp.array([2 / 3, 1]),
+        jnp.array([1 / 3, 1 / 2]),
+        jnp.array([1, 1 / 2]),
+        jnp.array([4 / 3, 1.0]),
+    )
 
     assert len(output) == 4
     assert jnp.all(jnp.isclose(output[0], expected[0]))
@@ -20,20 +26,22 @@ def test_sss_nig_normal():
     assert jnp.all(jnp.isclose(output[2], expected[2]))
     assert jnp.all(jnp.isclose(output[3], expected[3]))
 
+
 def test_sss_dirichlet_categorical():
-    alpha = (jnp.ones((2,2)),)
-    x = jnp.array([0,1,1,0,1], dtype=jax.numpy.int32)
-    assignments = jnp.array([0,0,1,1,1], dtype=jax.numpy.int32)
+    alpha = (jnp.ones((2, 2)),)
+    x = jnp.array([0, 1, 1, 0, 1], dtype=jax.numpy.int32)
+    assignments = jnp.array([0, 0, 1, 1, 1], dtype=jax.numpy.int32)
     output = ss._sss_dirichlet_categorical(alpha, x, assignments)
     expected = jnp.array([[1, 1], [1, 2]])
 
     assert len(output) == 1
     assert jnp.all(jnp.isclose(output[0], expected))
 
+
 def test_sss_beta_bernoulli():
-    parameters = (jnp.ones(2),jnp.ones(2))
-    x = jnp.array([0,1,1,0,1], dtype=jax.numpy.int32)
-    assignments = jnp.array([0,0,1,1,1], dtype=jax.numpy.int32)
+    parameters = (jnp.ones(2), jnp.ones(2))
+    x = jnp.array([0, 1, 1, 0, 1], dtype=jax.numpy.int32)
+    assignments = jnp.array([0, 0, 1, 1, 1], dtype=jax.numpy.int32)
     output = ss._sss_beta_bernoulli(parameters, x, assignments)
     expected = (jnp.array([2, 3]), jnp.array([2, 2]))
 
@@ -41,12 +49,13 @@ def test_sss_beta_bernoulli():
     assert jnp.all(jnp.isclose(output[0], expected[0]))
     assert jnp.all(jnp.isclose(output[1], expected[1]))
 
+
 def test_sss_gamma_poisson():
-    parameters = (jnp.ones(2),jnp.ones(2))
-    x = jnp.array([0,1,1,0,1], dtype=jax.numpy.int32)
-    assignments = jnp.array([0,0,1,1,1], dtype=jax.numpy.int32)
+    parameters = (jnp.ones(2), jnp.ones(2))
+    x = jnp.array([0, 1, 1, 0, 1], dtype=jax.numpy.int32)
+    assignments = jnp.array([0, 0, 1, 1, 1], dtype=jax.numpy.int32)
     output = ss._sss_gamma_poisson(parameters, x, assignments)
-    expected = (jnp.array([2, 3]), jnp.array([1/3, 1/4]))
+    expected = (jnp.array([2, 3]), jnp.array([1 / 3, 1 / 4]))
 
     assert len(output) == 2
     assert jnp.all(jnp.isclose(output[0], expected[0]))
@@ -54,33 +63,35 @@ def test_sss_gamma_poisson():
 
 
 def test_sss_normal_normal():
-    parameters = (jnp.zeros(2), jnp.ones(2), 2*jnp.ones(2))
-    x = jnp.array([0.0,1.0,1.0])
-    assignments = jnp.array([0,0,1], dtype=jax.numpy.int32)
+    parameters = (jnp.zeros(2), jnp.ones(2), 2 * jnp.ones(2))
+    x = jnp.array([0.0, 1.0, 1.0])
+    assignments = jnp.array([0, 0, 1], dtype=jax.numpy.int32)
     output = ss._sss_normal_normal(parameters, x, assignments)
-    expected = (jnp.array([1.0, 3/4]), jnp.array([1/2, 2/3]))
+    expected = (jnp.array([1.0, 3 / 4]), jnp.array([1 / 2, 2 / 3]))
 
     assert len(output) == 2
     assert jnp.all(jnp.isclose(output[0], expected[0]))
     assert jnp.all(jnp.isclose(output[1], expected[1]))
+
 
 def test_sss_gamma_normal():
     parameters = (jnp.ones(2), jnp.ones(2), jnp.zeros(2))
-    x = jnp.array([0.0,1.0,1.0])
-    assignments = jnp.array([0,0,1], dtype=jax.numpy.int32)
+    x = jnp.array([0.0, 1.0, 1.0])
+    assignments = jnp.array([0, 0, 1], dtype=jax.numpy.int32)
     output = ss._sss_gamma_normal(parameters, x, assignments)
-    expected = (jnp.array([2, 3/2]), jnp.array([3/2, 3/2]))
+    expected = (jnp.array([2, 3 / 2]), jnp.array([3 / 2, 3 / 2]))
 
     assert len(output) == 2
     assert jnp.all(jnp.isclose(output[0], expected[0]))
     assert jnp.all(jnp.isclose(output[1], expected[1]))
 
+
 def test_sss_inverse_gamma_normal():
     parameters = (jnp.ones(2), jnp.ones(2), jnp.zeros(2))
-    x = jnp.array([0.0,1.0,1.0])
-    assignments = jnp.array([0,0,1], dtype=jax.numpy.int32)
+    x = jnp.array([0.0, 1.0, 1.0])
+    assignments = jnp.array([0, 0, 1], dtype=jax.numpy.int32)
     output = ss._sss_inverse_gamma_normal(parameters, x, assignments)
-    expected = (jnp.array([2, 3/2]), jnp.array([3/2, 3/2]))
+    expected = (jnp.array([2, 3 / 2]), jnp.array([3 / 2, 3 / 2]))
 
     assert len(output) == 2
     assert jnp.all(jnp.isclose(output[0], expected[0]))
